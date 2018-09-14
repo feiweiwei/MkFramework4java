de72beedb90fafdf6b123dc6e86ce10dfa8c1ef1
feiweiwei <feiweiwei@shrbank.com>
2018-09-14 11:03:23 +0800

    安全－xss模块 0.6

---
+++ src/main/java/com/monkey01/common/config/FilterConfig.java
@@ -0,0 +1,31 @@
+package com.monkey01.common.config;
+
+import com.monkey01.common.xss.XssFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.DispatcherType;
+
+/**
+ * Filter配置
+ *
+ * @author feiweiwei
+ * @email feiweiwei@139.com
+ * @date 2017-04-21 21:56
+ */
+@Configuration
+public class FilterConfig {
+
+
+    @Bean
+    public FilterRegistrationBean xssFilterRegistration() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setDispatcherTypes(DispatcherType.REQUEST);
+        registration.setFilter(new XssFilter());
+        registration.addUrlPatterns("/*");
+        registration.setName("xssFilter");
+        registration.setOrder(Integer.MAX_VALUE);
+        return registration;
+    }
+}
